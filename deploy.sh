e63a19ad6a30545bc9885e377925abe0528e60c8
bin <17661977890@163.com>
2024-05-27 13:41:15 +0800

    feat:部署脚本

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -e
 
 
+
 push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
@@ -18,6 +19,7 @@ cd $dist_path
 git init
 git add -A
 git commit -m "deploy, $commit_info"
+echo "当前仓库：$push_addr 当前分支：$push_branch"
 git push -f $push_addr HEAD:$push_branch
 
 cd -
